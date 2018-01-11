872afccf0436312d729629b1d97cc711fff1e1bb
Jiajia Li <fruitfly1026@gmail.com>
2018-01-11 14:11:35 -0500

    minor

--- BreastCancer/run_csv.sh
+++ BreastCancer/run_csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../scripts/build_tensor.py test/1102.csv test/1103.csv out.tns \
+../scripts/build_tensor.py csvs/1102.csv csvs/1103.csv out.tns \
 	-f "post_author_id" --type="post_author_id",int \
 	-f "post_id" --type="post_id",int \
 	-f "post_time" --type="post_time",float
--- BreastCancer/run_gz.sh
+++ BreastCancer/run_gz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../scripts/build_tensor.py test/1102.csv.gz test/1103.csv.gz out.tns \
+../scripts/build_tensor.py csvs/1102.csv.gz csvs/1103.csv.gz out.tns \
 	-f "post_author_id" --type="post_author_id",int \
 	-f "post_id" --type="post_id",int \
 	-f "post_time" --type="post_time",float
