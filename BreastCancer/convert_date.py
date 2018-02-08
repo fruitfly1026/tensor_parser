43ae957be1a6878f4bbc66ca43c5809d9ca3590b
Jiajia Li <fruitfly1026@gmail.com>
2018-02-08 14:57:40 -0500

    add processing dates

---
+++ BreastCancer/convert_date.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import gzip
+from datetime import datetime
+
+# in_gz_file = "../example_csvs/test.csv.gz"
+# out_gz_file = "../example_csvs/converted_test.csv.gz"
+in_gz_file = "/scratch/jli458/BIGTENSORS/BreastCancer/merged2.csv.gz"
+out_gz_file = "/scratch/jli458/BIGTENSORS/BreastCancer/converted_merged2.csv.gz"
+
+with gzip.open(out_gz_file, 'wb') as fo:
+	with gzip.open(in_gz_file, 'rb') as fi:
+		for line in fi:
+			# print(line.decode('utf8'))
+			words = line.decode('utf8').split(",")
+			if words[0] != "post_thread_id":
+				try:
+					tmp_time = datetime.strptime(words[3], "%Y-%m-%d %H:%M:%S")
+					words[3] = str(tmp_time.timestamp())
+					out_str = ",".join(words)
+					print(line.decode('utf8'))
+					fo.write(out_str.encode())
+				except ValueError:
+					fo.write(line)
+					# print("float numbers for dates.")
+			else:
+				fo.write(line)			
+		
